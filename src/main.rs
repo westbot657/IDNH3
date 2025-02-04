fa74d6be72b6712aec22d1ae9a7296a7eb6dde6e
brightoncox <brightonlcox@gmail.com>
2025-02-04 14:15:27 -0700

    removed window decorations

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                decorations: false,
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .run();
 }
