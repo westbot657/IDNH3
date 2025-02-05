a0af7fd5b2fc59bd01007b3cb3bf36d4247fc547
westbot <82739021+westbot657@users.noreply.github.com>
2025-02-05 00:06:46 -0700

    added textures

--- src/main.rs
+++ src/main.rs
@@ -9,5 +9,44 @@ fn main() {
             }),
             ..Default::default()
         }))
+        .add_systems(Startup, setup)
         .run();
+}
+
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>
+) {
+    let idnh_icon = asset_server.load("textures/idnh_icon.png");
+
+    let mut mesh = Mesh::from(Rectangle::default());
+
+    let vertex_colors: Vec<[f32; 4]> = vec![
+        LinearRgba::RED.to_f32_array(),
+        LinearRgba::GREEN.to_f32_array(),
+        LinearRgba::BLUE.to_f32_array(),
+        LinearRgba::WHITE.to_f32_array(),
+    ];
+
+    mesh.insert_attribute(Mesh::ATTRIBUTE_COLOR, vertex_colors);
+
+    let mesh_handle = meshes.add(mesh);
+
+    commands.spawn(Camera2d);
+
+    commands.spawn((
+        Mesh2d(mesh_handle.clone()),
+        MeshMaterial2d(materials.add(ColorMaterial::default())),
+        Transform::from_translation(Vec3::new(-96., 0., 0.)).with_scale(Vec3::splat(128.)),
+    ));
+
+    commands.spawn((
+        Mesh2d(mesh_handle),
+        MeshMaterial2d(materials.add(idnh_icon)),
+        Transform::from_translation(Vec3::new(96., 0., 0.)).with_scale(Vec3::splat(128.)),
+    ));
+
 }
